6708c1712a25bbe26ef24087558718803b9d67a8
unknown-user <unknown-user@192.168.2.127>
2015-04-16 10:14:41 +0100

    Initial check-in of module MultipageApp

---
+++ MultipageApp/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace MultipageApp
+{
+	[Activity (Label = "MultipageApp", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			SetContentView (Resource.Layout.Main);
+
+			Button aboutUs = FindViewById<Button> (Resource.Id.about);
+
+			aboutUs.Click += (object sender, EventArgs e) => {
+				var intent = new Intent(this, typeof(AboutUsActivity));
+
+				StartActivity(intent);
+			};
+		}
+	}
+}
